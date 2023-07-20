168676c33f69e9f56754dae562d9b9b4b341354d
김진아 <c2010333@naver.com>
2023-07-20 22:06:02 +0900

    lv_3

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,31 +1,53 @@
 package com.example.Calculator
 
-class Calculator() {
-    val welcome = println("계산기를 실행했습니다.")
+import com.example.calculator.AddOperation
+import com.example.calculator.DivideOperation
+import com.example.calculator.MultiplyOperation
+import com.example.calculator.SubstractOperation
+
+fun main() {
+    val welcome = println("계산기를 실행합니다.")
     val gate = println("==============================")
 
     val first = println("첫 번째 숫자를 작성해주세요.")
-    var num1 = readLine()!!.toInt()
+    val num1 = readLine()!!.toInt()
 
-    val second = println("연산 기호를 작성해주세요.")
-    var operator = readLine()!!.toString()
+    val write = println("연산 기호를 작성해주세요.")
+    val operator = readLine()!!.toString()
 
-    val last = println("두 번째 숫자를 작성해주세요.")
-    var num2 = readLine()!!.toInt()
+    val second = println("두 번째 숫자를 작성해주세요.")
+    val num2 = readLine()!!.toInt()
 
-    fun Operation() {
-        if (operator == "+") {
-            println("결과는 ${num1 + num2} 입니다.")
-        } else if (operator == "-") {
-            println("결과는 ${num1 - num2} 입니다.")
-        } else if (operator == "*") {
-            println("결과는 ${num1 * num2} 입니다.")
-        } else if (operator == "/") {
-            println("결과는 ${num1 / num2} 입니다.")
-        } else if (operator == "%") {
-            println("결과는 ${num1 % num2} 입니다.")
-        } else {
-            println("잘못된 부분이 있습니다. 다시 입력해주세요.")
+    val addOperation = AddOperation()
+    val result = addOperation.operate(num1, num2)
+    val last = "$num1 $operator $num2 의 값은 ${result}입니다."
+
+    val substractOperation = SubstractOperation()
+    val result2 = substractOperation.operate(num1, num2)
+    val lasta = "$num1 $operator $num2 의 값은 ${result2}입니다."
+
+    val multiplyOperation = MultiplyOperation()
+    val result3 = multiplyOperation.operate(num1, num2)
+    val lastb = "$num1 $operator $num2 의 값은 ${result3}입니다."
+
+    val divideOperation = DivideOperation()
+    val result4 = divideOperation.operate(num1, num2)
+    val lastc = "$num1 $operator $num2 의 값은 ${result4}입니다."
+
+    when (operator) {
+        "+" -> {
+            println("$last")
+        }
+        "-" -> {
+            println("$lasta")
+        }
+        "*" -> {
+            println("$lastb")
+        }
+        "/" -> {
+            println("$lastc")
         }
     }
 }
+
+
