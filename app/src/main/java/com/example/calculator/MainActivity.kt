15b3775397152633047158bd0ccfeb22b63c83ca
김진아 <c2010333@naver.com>
2023-07-20 21:59:43 +0900

    lv_2

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,23 +1,31 @@
-fun main() {
-    Calculator().Operation()
-}
+package com.example.Calculator
 
-open class Calculator() {
-    val print = "숫자와 연산기호를 작성해주세요."
+class Calculator() {
+    val welcome = println("계산기를 실행했습니다.")
+    val gate = println("==============================")
+
+    val first = println("첫 번째 숫자를 작성해주세요.")
     var num1 = readLine()!!.toInt()
+
+    val second = println("연산 기호를 작성해주세요.")
     var operator = readLine()!!.toString()
+
+    val last = println("두 번째 숫자를 작성해주세요.")
     var num2 = readLine()!!.toInt()
 
     fun Operation() {
-        if(operator == "+") {
-            println(num1 + num2)
-        } else if(operator == "-") {
-            println(num1 - num2)
-        } else if(operator == "*") {
-            println(num1 * num2)
-        } else if(operator == "/") {
-            println(num1 / num2)
+        if (operator == "+") {
+            println("결과는 ${num1 + num2} 입니다.")
+        } else if (operator == "-") {
+            println("결과는 ${num1 - num2} 입니다.")
+        } else if (operator == "*") {
+            println("결과는 ${num1 * num2} 입니다.")
+        } else if (operator == "/") {
+            println("결과는 ${num1 / num2} 입니다.")
+        } else if (operator == "%") {
+            println("결과는 ${num1 % num2} 입니다.")
+        } else {
+            println("잘못된 부분이 있습니다. 다시 입력해주세요.")
         }
     }
-
 }
